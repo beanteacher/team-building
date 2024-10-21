94ec0ee029dd83bbbc8275de3e3e810802a4fee9
kookong2 <hy010309@naver.com>
2024-10-21 14:59:00 +0900

    [FEAT] 관리자 회원 상세 조회 서비스 코드 구현

--- team/src/main/java/sansam/team/user/query/service/AdminUserInfoQueryService.java
+++ team/src/main/java/sansam/team/user/query/service/AdminUserInfoQueryService.java
@@ -3,6 +3,7 @@ package sansam.team.user.query.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sansam.team.user.query.dto.AdminUserInfoAllQueryDTO;
+import sansam.team.user.query.dto.AdminUserInfoQueryDTO;
 import sansam.team.user.query.mapper.AdminUserInfoQueryMapper;
 
 import java.util.List;
@@ -14,11 +15,12 @@ public class AdminUserInfoQueryService {
     private final AdminUserInfoQueryMapper adminUserInfoQueryMapper;
 
     /* 관리자 회원 전체 조회 */
-
     public List<AdminUserInfoAllQueryDTO> getAllUserInfo() {
         return adminUserInfoQueryMapper.findAllUserInfo();
     }
 
-    /* 관리자 회원 상세 조회 */
-    /* 이미지URL 이름 아이디 닉네임 이메일 회원유형 상태 가입일 전화번호 생년월일 성별 전공 경력일수 깃허브아이디 참여중인 프로젝트 */
+    /* 관리자 회원 상세 조회 (userSeq를 기반으로 조회) */
+    public AdminUserInfoQueryDTO getUserInfo(Long userSeq){
+        return adminUserInfoQueryMapper.findUserInfo(userSeq);
+    }
 }
