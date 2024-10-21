e4499af785ccf551227a166f87c7a242653e1588
kookong2 <hy010309@naver.com>
2024-10-21 14:58:17 +0900

    [FEAT] 관리자 회원 상세 조회 기능 구현

--- team/src/main/java/sansam/team/user/query/controller/AdminUserInfoQueryController.java
+++ team/src/main/java/sansam/team/user/query/controller/AdminUserInfoQueryController.java
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sansam.team.common.response.ApiResponse;
 import sansam.team.user.query.dto.AdminUserInfoAllQueryDTO;
+import sansam.team.user.query.dto.AdminUserInfoQueryDTO;
 import sansam.team.user.query.service.AdminUserInfoQueryService;
 
 import java.util.List;
@@ -30,4 +32,12 @@ public class AdminUserInfoQueryController {
     }
 
 
+//    관리자 회원 상세 조회
+    @GetMapping("/{userSeq}")
+    @Operation(summary = "관리자 유저 정보 상세 조회", description = "관리자 유저 정보 상세 조회 API (관리자만 가능)")
+    public ApiResponse<AdminUserInfoQueryDTO> getUserInfo(@PathVariable Long userSeq) {
+
+        AdminUserInfoQueryDTO userInfo = adminUserInfoQueryService.getUserInfo(userSeq);
+        return ApiResponse.ofSuccess(userInfo);
+    }
 }
