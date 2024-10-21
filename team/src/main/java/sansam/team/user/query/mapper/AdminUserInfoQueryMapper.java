5f26c1860ba601b5201d37808be69071aa171846
kookong2 <hy010309@naver.com>
2024-10-21 14:58:48 +0900

    [FEAT] AdminUserInfoQueryMapper 관리자 회원 상세 조회 구현

--- team/src/main/java/sansam/team/user/query/mapper/AdminUserInfoQueryMapper.java
+++ team/src/main/java/sansam/team/user/query/mapper/AdminUserInfoQueryMapper.java
@@ -1,7 +1,9 @@
 package sansam.team.user.query.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import sansam.team.user.query.dto.AdminUserInfoAllQueryDTO;
+import sansam.team.user.query.dto.AdminUserInfoQueryDTO;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface AdminUserInfoQueryMapper {
 
 
     List<AdminUserInfoAllQueryDTO> findAllUserInfo();
+
+    AdminUserInfoQueryDTO findUserInfo(@Param("userSeq") Long userSeq);
 }
