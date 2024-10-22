252a83c2234bb3f08ffc9c1b29520ba0239daf0b
kookong2 <hy010309@naver.com>
2024-10-22 15:29:36 +0900

    [FEAT] TeamService 팀 추가 기능 구현

--- team/src/main/java/sansam/team/team/command/application/service/TeamService.java
+++ team/src/main/java/sansam/team/team/command/application/service/TeamService.java
@@ -8,11 +8,13 @@ import org.springframework.transaction.annotation.Transactional;
 import sansam.team.exception.CustomException;
 import sansam.team.exception.ErrorCodeType;
 import sansam.team.team.command.application.dto.TeamCreateRequest;
+import sansam.team.team.command.application.dto.TeamDTO;
 import sansam.team.team.command.application.dto.TeamUpdateRequest;
 import sansam.team.team.command.domain.aggregate.entity.Team;
+import sansam.team.team.command.domain.aggregate.entity.TeamMember;
+import sansam.team.team.command.domain.repository.TeamMemberRepository;
 import sansam.team.team.command.domain.repository.TeamRepository;
 import sansam.team.team.command.domain.repository.TeamScheduleRepository;
-import sansam.team.team.command.mapper.TeamMapper;
 
 @Slf4j
 @Service
@@ -20,16 +22,34 @@ import sansam.team.team.command.mapper.TeamMapper;
 public class TeamService {
 
     private final TeamRepository teamRepository;
-
     private final TeamScheduleRepository teamScheduleRepository;
-
+    private final TeamMemberRepository teamMemberRepository;
     private final ModelMapper modelMapper;
+
+    @Transactional
+    public void createTeams(TeamCreateRequest teamCreateRequest) {
+
+        Long projectSeq = teamCreateRequest.getProjectSeq();
+
+        for (TeamDTO teamDTO : teamCreateRequest.getTeams()) {
+            // ModelMapper로 TeamDTO를 Team 엔티티로 변환
+            Team team = modelMapper.map(teamDTO, Team.class);
+            team.ProjectSeq(projectSeq);  // projectSeq는 수동으로 설정
+            teamRepository.save(team);
+
+            // userSeq를 기반으로 팀 멤버 추가
+            for (Long userSeq : teamDTO.getUserSeqs()) {
+                TeamMember teamMember = new TeamMember(userSeq, team.getTeamSeq());
+                teamMemberRepository.save(teamMember);
+            }
+        }
+    }
 
     @Transactional
     public Team updateTeam(Long teamSeq, TeamUpdateRequest request) {
         Team team = teamRepository.findById(teamSeq).orElseThrow();
 
-        team.modifyTeam(request.getRuleSeq(), request.getTeamName());
+        team.modifyTeam(request.getTeamName());
 
         return team;
     }
