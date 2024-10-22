95316eed6339d62f83c19ba62c98c3f4b72cf2d1
kookong2 <hy010309@naver.com>
2024-10-22 16:44:00 +0900

    [FEAT] 회원 평가 상세 조회 서비스 기능 구현

--- team/src/main/java/sansam/team/team/query/service/TeamUserReviewQueryService.java
+++ team/src/main/java/sansam/team/team/query/service/TeamUserReviewQueryService.java
@@ -2,7 +2,8 @@ package sansam.team.team.query.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import sansam.team.team.query.dto.UserReviewAllQueryDTO;
+import sansam.team.team.query.dto.TeamUserReviewAllQueryDTO;
+import sansam.team.team.query.dto.TeamUserReviewQueryDTO;
 import sansam.team.team.query.mapper.TeamUserReviewQueryMapper;
 
 import java.util.List;
@@ -14,9 +15,12 @@ public class TeamUserReviewQueryService {
     private final TeamUserReviewQueryMapper teamUserReviewQueryMapper;
 
     /* 회원 평가 전체 조회*/
-    public List<UserReviewAllQueryDTO> getUserAllReview(){
+    public List<TeamUserReviewAllQueryDTO> getUserAllReview(){
         return teamUserReviewQueryMapper.findUserAllReview();
     }
 
     /* 회원 평가 상세 조회 */
+    public TeamUserReviewQueryDTO getUserReview(Long userReviewSeq){
+        return teamUserReviewQueryMapper.findUserReview(userReviewSeq);
+    }
 }
