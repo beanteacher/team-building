25881b2a98e29c9fe7cb5b6b361a247d2addc79c
beanteacher <99578261+beanteacher@users.noreply.github.com>
2024-10-24 18:13:39 +0900

    [REFACTOR] 채팅방 진입 시 조회 내용 변경

--- team/src/main/java/sansam/team/team/query/dto/TeamFindByIdResponse.java
+++ team/src/main/java/sansam/team/team/query/dto/TeamFindByIdResponse.java
@@ -2,14 +2,17 @@ package sansam.team.team.query.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 public class TeamFindByIdResponse {
     private Long teamSeq;
     private String teamName;
+    private TeamChatResponse teamChatResponse;
     private List<TeamMemberResponse> teamMemberList;
     private List<TeamScheduleResponse> teamScheduleList;
 }
