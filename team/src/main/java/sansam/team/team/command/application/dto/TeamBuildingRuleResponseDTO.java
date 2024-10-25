0c35731c9340bd3e6d020f5a0bf9d347b3335e58
person76 <94957124+person76@users.noreply.github.com>
2024-10-25 19:47:25 +0900

    [REFACTOR] 팀 빌딩 규칙 생성 반환 DTO생성
    [REFACTOR] 팀 빌딩 규칙 생성 반환 DTO생성

---
+++ team/src/main/java/sansam/team/team/command/application/dto/TeamBuildingRuleResponseDTO.java
@@ -0,0 +1,14 @@
+package sansam.team.team.command.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeamBuildingRuleResponseDTO {
+    private Long teamBuildingRuleSeq;
+}
