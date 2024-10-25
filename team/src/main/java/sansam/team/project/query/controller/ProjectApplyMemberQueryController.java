7320b970ae86a18762e24aa297f100e326491bb0
kookong2 <hy010309@naver.com>
2024-10-25 14:38:42 +0900

    [REFACTOR] 프로젝트 신청 회원 엔드포인트 변경

--- team/src/main/java/sansam/team/project/query/controller/ProjectApplyMemberQueryController.java
+++ team/src/main/java/sansam/team/project/query/controller/ProjectApplyMemberQueryController.java
@@ -14,7 +14,7 @@ import sansam.team.project.query.service.ProjectApplyMemberQueryService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/project/board/{projectBoardSeq}/apply")
+@RequestMapping("/api/v1/project/board/apply")
 @RequiredArgsConstructor
 @Tag(name = "2-2. Project Board Apply Member API", description = "프로젝트 게시물 신청 회원 API")
 public class ProjectApplyMemberQueryController {
@@ -23,8 +23,8 @@ public class ProjectApplyMemberQueryController {
 
     // 특정 회원이 신청한 프로젝트 목록 전체 조회 API
     @GetMapping()
-    @Operation(summary = "프로젝트 신청 리스트 조회", description = "해당 회원이 신청한 프로젝트 리스트 조회")
-    public ApiResponse<List<ApplyMemberQueryDTO>> getMyProjects(@PathVariable Long projectBoardSeq) {
+    @Operation(summary = "회원의 프로젝트 신청 리스트 조회", description = "해당 회원이 신청한 프로젝트 리스트 조회")
+    public ApiResponse<List<ApplyMemberQueryDTO>> getMyProjects() {
         List<ApplyMemberQueryDTO> projects = projectApplyMemberQueryService.findProjectsByCurrentUser();
         return ApiResponse.ofSuccess(projects);
     }
